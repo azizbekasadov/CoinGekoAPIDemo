4b126244518dc7de0c5ca991ac9b5760ab3dad9f
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-11-20 01:57:15 +0300

    Initial Commit

---
+++ CoinGekoAPIDemo/CoinGekoAPIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGekoAPIDemoApp.swift
+//  CoinGekoAPIDemo
+//
+//  Created by Azizbek Asadov on 20.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinGekoAPIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
